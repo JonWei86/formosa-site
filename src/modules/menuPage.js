cb065d2fd19d037819f0fa93a93c627c8c238079
jon <jkwei.86@gmail.com>
2024-11-07 19:59:24 -0500

    feat: adds price to menu

--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -2,6 +2,6 @@ export const menuPage = () =>{
     const content = document.querySelector('#content');
 
     const story = document.createElement('p');
-    story.textContent = 'porkbelly taco'
+    story.textContent = 'porkbelly taco...........$15.99'
     content.appendChild(story);
 }
